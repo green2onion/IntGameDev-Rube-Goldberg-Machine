3cc5f7e4ee90248ff3a377a2b3bb91d0e4360420
Zeru Hu <green2onion@gmail.com>
2020-09-30 00:13:24 +0800

    first version

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject toFollow;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+        Vector3 position = transform.position;
+        position.x = Mathf.Lerp(transform.position.x, toFollow.transform.position.x, interpolation);
+        position.y = Mathf.Lerp(transform.position.y, toFollow.transform.position.y - 2, interpolation);
+        position.z = Mathf.Lerp(transform.position.z, toFollow.transform.position.z - 10, interpolation);
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            Destroy(gameObject);
+        }
+    }
+}
